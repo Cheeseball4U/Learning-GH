5ac4e759aa312f909fbc1e77b389b35125607e4b
Constantine <costamess@icloud.com>
2023-02-28 09:49:10 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Constantine Messimeris on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
